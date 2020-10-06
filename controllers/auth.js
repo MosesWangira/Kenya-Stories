e519d23216c6f4d56e8eba9d12c04fe89a890988
MosesWangira <40572173+MosesWangira@users.noreply.github.com>
2020-10-06 11:34:23 +0300

    fix sign up bug

--- controllers/auth.js
+++ controllers/auth.js
@@ -13,7 +13,11 @@ using bcrypt fo password hashing
 exports.signup = (req, res) => {
   const signup = new SignUp(req.body);
 
-  console.log("Sign up: " + SignUp);
+  var db = req.db;
+  var collection = db.get('SignUp');
+
+  console.log(db);
+
   //
   // signup.save().then(result => {
   //   res.status(200).json({
