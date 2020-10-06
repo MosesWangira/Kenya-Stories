3e8c836ca1a7a6839894a5feb7d7b6dbeee4d799
MosesWangira <40572173+MosesWangira@users.noreply.github.com>
2020-10-06 12:48:21 +0300

    fix sign up bug

--- controllers/auth.js
+++ controllers/auth.js
@@ -14,84 +14,27 @@ using bcrypt fo password hashing
 exports.signup = (req, res) => {
   const signup = new SignUp(req.body);
 
+  bcrypt.hash(signup.password, saltRounds, (err, hash) => {
 
-  const signUpSchema = new SignUp({
-    name: signup.name,
-    email: signup.email,
-    password: "hashed password",
-    resetPasswordLink: signup.resetPasswordLink,
-    emailConfirmation: signup.emailConfirmation
-  });
+    //Data to be saved to database and sent as json
+    const signUpSchema = new SignUp({
+      name: signup.name,
+      email: signup.email,
+      password: hash,
+      resetPasswordLink: signup.resetPasswordLink,
+      emailConfirmation: signup.emailConfirmation
+    });
 
-  //save to database
-  signUpSchema.save().then(result => {
-    res.status(200).json({
-      status: 200,
-      result: result
+    //save to database
+    signUpSchema.save().then(result => {
+      res.status(200).json({
+        status: 200,
+        result: result
+      })
     })
+
+
   })
-
-  // const query = {'email': signup.email}
-  //
-  // const signUpCollection = mongoose.model('SignUp', signUpSchema);
-
-  // find each person with a last name matching 'Ghost', selecting the `name` and `occupation` fields
-  // signUpCollection.findOne({ 'name.last': 'Ghost' }, 'name occupation', function (err, person) {
-  //   if (err) return handleError(err);
-  //   // Prints "Space Ghost is a talk show host".
-  //   console.log('%s %s is a %s.', person.name.first, person.name.last,
-  //   person.occupation);
-  // });
-
-  //
-  // signup.save().then(result => {
-  //   res.status(200).json({
-  //     status: 200,
-  //     result: result
-  //   })
-  // })
-
-  // bcrypt.hash(signup.password, saltRounds, (err, hash) => {
-
-    // const newUser = {
-    //   name: signup.name,
-    //   email: signup.email,
-    //   password: hash,
-    //   emailConfirmation: false,
-    //   resetPasswordLink: ""
-    // }
-
-    // signUpCollection.findOne(query, (err, result) => {
-    //   if(err){
-    //     //status 400 is failed response
-    //     const jsonObjectError = {
-    //       status: 400,
-    //       error: err
-    //     }
-    //     console.log(err);
-    //     res.status(400).send(jsonObjectError)
-    //   }
-    //   if(result == null){
-    //     //save to database
-    //     signup.save().then(result => {
-    //       res.status(200).json({
-    //         status: 200,
-    //         result: result
-    //       })
-    //     })
-    //   }else {
-    //     //status 400 is failed response
-    //     const jsonObjectError = {
-    //       status: 400,
-    //       error: 'email already registered'
-    //     }
-    //     console.log(err);
-    //     res.status(400).send(jsonObjectError)
-    //   }
-    // })
-
-
-  // })
 
 };
 
