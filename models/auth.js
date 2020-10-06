7eea853ade16c894ee60f16a27640dce8fab21ee
MosesWangira <40572173+MosesWangira@users.noreply.github.com>
2020-10-06 11:08:19 +0300

    fix sign up bug

--- models/auth.js
+++ models/auth.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose')
 
 const authSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
   email: {
     type: String,
     required: true
@@ -12,9 +15,6 @@ const authSchema = new mongoose.Schema({
   resetPasswordLink: {
     type: String,
     default: ""
-  },
-  name: {
-    type: String,
   },
   emailConfirmation: {
     type: Boolean,
