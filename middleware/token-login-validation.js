828bba3812ca1448fba7dcd98f4f7a28fb85dba7
MosesWangira <40572173+MosesWangira@users.noreply.github.com>
2020-11-23 06:18:20 +0300

    return status 200 if token is valid

--- middleware/token-login-validation.js
+++ middleware/token-login-validation.js
@@ -2,10 +2,18 @@ const jwt = require('jsonwebtoken')
 
 module.exports = (req, res, next) =>{
   try{
+
+    const jsonResponse = {
+      status: 400,
+      error: "token is valid"
+    }
+
     const token = req.headers.authorization.split(" ")[1]
     const decoded = jwt.verify(token, process.env.JWT_KEY)
     req.userData = decoded
     next()
+
+    res.status(200).send(jsonResponse)
   }catch(error){
     const jsonResponse = {
       status: 400,
